31196972812f6dea97e6fee81b98df1f48b250cf
Ofurosuki <ryo-yoshida@keio.jp>
2023-04-09 18:34:50 +0900

    コンフィグだけ書いた

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#include <mbed.h>
+
+#define PWM_1 PA_12
+#define PWM_2 PB_5
+#define DIR_1 PB_0
+#define DIR_2 PB_1
+
+// #define A  B
+// とは、プログラムのコンパイル前に、自ら定義した文字式Aを定数Bに置き換える処理です。
+// ピン配置など、基本変わらないものを定義しておくと、後々の変更が楽になる。
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
+#include <config.h>
 #include <mbed.h>
 
 int main() {
-
   // put your setup code here, to run once:
 
-  while(1) {
+  while (1) {
     // put your main code here, to run repeatedly:
   }
 }
