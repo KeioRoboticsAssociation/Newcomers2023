5d4299b9d332be36254b3077be699ed8d31a1e40
Ofurosuki <ryo-yoshida@keio.jp>
2023-05-08 17:32:44 +0900

    sample

--- include/config.h
+++ include/config.h
@@ -1,10 +1,20 @@
 #include <mbed.h>
+// ここで変数を宣言すると、config.h をインクルード
+// すれば他のファイルからも参照できる。
+
+extern AnalogIn stick_x;
+extern AnalogIn stick_y;
+extern PwmOut pwm_l;
+extern PwmOut pwm_r;
+extern DigitalOut dir_l;
+extern DigitalOut dir_r;
 
 #define PWM_1 PA_12
 #define PWM_2 PB_5
 #define DIR_1 PB_0
 #define DIR_2 PB_1
-
+#define An_1 PA_0
+#define An_2 PA_1
 // #define A  B
 // とは、プログラムのコンパイル前に、自ら定義した文字式Aを定数Bに置き換える処理です。
 // ピン配置など、基本変わらないものを定義しておくと、後々の変更が楽になる。
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,28 @@
 
 int main() {
   // put your setup code here, to run once:
+  const float deadzone = 0.05f;  // deadzone
+  // const float distance = 0.5f;   // m
+  float l = 0.0f;
+  float r = 0.0f;
+  // float v = 0.0f;
+  // float omega = 0.0f;
 
   while (1) {
-    // put your main code here, to run repeatedly:
+    l = stick_x - 0.5f;  // -0.5<l<0.5
+    r = stick_y - 0.5f;
+    // v = sqrt(x * x + y * y);
+    // omega = atan2(y, x);
+    if (l > deadzone || l < -deadzone) {
+      if (l > 0.0f) {
+        dir_l = 1;
+        pwm_l = l;
+      } else {
+        dir_l = 0;
+        pwm_l = -l;
+      }
+    } else {
+      pwm_l = 0.0f;
+    }
   }
 }
