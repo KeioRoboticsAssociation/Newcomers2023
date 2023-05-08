4d5cce6f445d225d4714a0ec5e8fc4b6e535f552
Ofurosuki <ryo-yoshida@keio.jp>
2023-05-08 17:42:11 +0900

    new

--- include/config.h
+++ include/config.h
@@ -4,6 +4,7 @@
 #define PWM_2 PB_5
 #define DIR_1 PB_0
 #define DIR_2 PB_1
+#define LED LED1
 
 // #define A  B
 // とは、プログラムのコンパイル前に、自ら定義した文字式Aを定数Bに置き換える処理です。
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 
 int main() {
   // put your setup code here, to run once:
-
+  DigitalOut led(LED);  // 型名　変数名(ピン名)
   while (1) {
+    led = 1;       // 1を代入すると、LEDが点灯するs
+    wait_us(100);  // wait_us(x) --> x[us]待つそして次の行へ進む
     // put your main code here, to run repeatedly:
   }
 }
